3d9f97df1768e46ba31454f91ad13b21362041c9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2020-02-02 16:45:08 +0100

    Autowiring to field

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,15 +1,13 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleApplication {
 
+    @Autowired
     private SkypeMessageService messageService;
-
-    public SimpleApplication(SkypeMessageService messageService) {
-        this.messageService = messageService;
-    }
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
