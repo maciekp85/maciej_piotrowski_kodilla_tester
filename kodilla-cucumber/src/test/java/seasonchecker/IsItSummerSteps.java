7943e83dff72d3cf3f927db3e2435b9a04f69228
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2020-03-01 15:52:56 +0100

    14.2 Adding second scenario

--- kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
+++ kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
@@ -3,14 +3,20 @@ package seasonchecker;
 import io.cucumber.java8.En;
 import org.junit.Assert;
 
+import java.time.MonthDay;
+
 public class IsItSummerSteps implements En {
 
-    private String today;
+    private MonthDay today;
     private String answer;
 
     public IsItSummerSteps() {
         Given("today is last day of December", () -> {
-            this.today = "last day of December";
+            this.today = MonthDay.of(12, 31);
+        });
+
+        Given("today is first day of August", () -> {
+            this.today = MonthDay.of(9, 1);
         });
 
         When("I ask whether it's Summer", () -> {
